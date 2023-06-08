7b87f01de7627f249c692663227b015db86cf2d4
Andrew Breytenbach <ambreyts15@gmail.com>
2023-06-08 08:56:45 -0400

    updated the index.js file to improve the functionality of the code

--- Develop/client/src/js/index.js
+++ Develop/client/src/js/index.js
@@ -1,6 +1,6 @@
 import { Workbox } from 'workbox-window';
 import Editor from './editor';
-import './database';
+import { initializeDatabase } from './database';
 import '../css/style.css';
 
 const main = document.querySelector('#main');
@@ -10,24 +10,32 @@ const loadSpinner = () => {
   const spinner = document.createElement('div');
   spinner.classList.add('spinner');
   spinner.innerHTML = `
-  <div class="loading-container">
-  <div class="loading-spinner" />
-  </div>
+    <div class="loading-container">
+      <div class="loading-spinner"></div>
+    </div>
   `;
   main.appendChild(spinner);
 };
 
-const editor = new Editor();
+const initApp = async () => {
+  const editor = new Editor();
 
-if (typeof editor === 'undefined') {
-  loadSpinner();
-}
+  if (typeof editor === 'undefined') {
+    loadSpinner();
+  }
+};
+
+// Initialize the database
+initializeDatabase();
 
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
-  // register workbox service worker
+  // Register the workbox service worker
   const workboxSW = new Workbox('/src-sw.js');
   workboxSW.register();
 } else {
   console.error('Service workers are not supported in this browser.');
 }
+
+// Call the initApp function after the DOM has loaded
+document.addEventListener('DOMContentLoaded', initApp);
