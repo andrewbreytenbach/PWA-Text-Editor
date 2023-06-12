eca677d29c877cfac0899024a6b54673fe13f66b
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-06-12 07:45:34 -0400

    updated the webdpack.config.js file with both comments describing the functionality of the code and also with the configuration attributes fingerprints, inject, and publicpath

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -14,37 +14,46 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      // Generates an HTML file from a template and injects the generated bundles
       new HtmlWebpackPlugin({
         template: './index.html',
       }),
 
-    new WebpackPwaManifest({
-      name: 'JATE Text Editor',
-      short_name: 'JATE',
-      description: 'A text editor for creating and storing notes or code snippets.',
-      background_color: '#ffffff',
-      theme_color: '#000000',
-      start_url: '/',
-      icons: [
-        {
-          src: path.resolve('src/images/icon.png'),
-          sizes: [96, 128, 192, 256, 384, 512],
-          destination: path.join('assets', 'icons'),
-        },
-      ],
-    }),
-    new InjectManifest({
-      swSrc: './src/js/service-worker.js',
-      swDest: 'service-worker.js',
-    }),
-  ],
+      // Generates a Web App Manifest file for the Progressive Web App (PWA)
+      new WebpackPwaManifest({
+        name: 'JATE Text Editor',
+        short_name: 'JATE',
+        description: 'A text editor for creating and storing notes or code snippets.',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        start_url: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+        fingerprints: true, // Add fingerprints to generated assets
+        inject: true, // Inject the manifest into the HTML file
+        publicPath: '/', // Specify the public path for assets referenced in the manifest
+      }),
+
+      // Injects a service worker into the webpack build
+      new InjectManifest({
+        swSrc: './src/js/service-worker.js',
+        swDest: 'service-worker.js',
+      }),
+    ],
 
     module: {
       rules: [
+        // Handles CSS files
         {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
         },
+        // Transpiles JavaScript files using Babel
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
