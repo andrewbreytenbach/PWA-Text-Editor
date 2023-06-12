6688ab7840fb8d56048e698c1b76486d6f9fd23d
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-06-12 09:12:08 -0400

    finally fixed the issue with the heroku deployment

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,14 +1,19 @@
 // Import the necessary modules
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+import { offlineFallback, warmStrategyCache } from 'workbox-recipes';
+import { CacheFirst, StaleWhileRevalidate } from 'workbox-strategies';
+import { registerRoute } from 'workbox-routing';
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+import { ExpirationPlugin } from 'workbox-expiration';
+import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
 
 // Define the array of files to precache
-const precacheFiles = [
-  // Add the list of files to be precached here
+const precacheFiles = self.__WB_MANIFEST || [
+  './index.html',
+  './src/images/icon.png',
+  './src/js/editor.js',
+  './src/js/database.js',
+  './src/js/index.js',
+  './src/css/style.css',
 ];
 
 // Precache and route the specified files using the precacheFiles array
