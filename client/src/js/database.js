925f176ebf60a9a3723c8573fa1cb54b427c3e18
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-06-12 07:24:41 -0400

    updated the database.js file to include comments describing the functionality of the code and also to update the syntax and semantics for the putDb and getDb methods for storing and retrieving data

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,37 +1,50 @@
 import { openDB } from 'idb';
 
+// Function to initialize the database
 const initDB = async () => {
   const db = await openDB('jate', 1, {
+    // The upgrade function is called when the database is created or needs to be upgraded
     upgrade(db) {
+      // Check if the 'jate' object store already exists in the database
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
+      
+      // Create a new object store named 'jate' with a keyPath of 'id' and auto-incrementing IDs
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
-  return db;
+  return db; // Return the database object
 };
 
+// Function to store data in the database
 export const putDb = async (content) => {
-  const db = await initDB();
-  const tx = db.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
+  const db = await initDB(); // Initialize the database
+  const tx = db.transaction('jate', 'readwrite'); // Start a transaction in 'readwrite' mode
+  const store = tx.objectStore('jate'); // Get the object store named 'jate'
+  
+  // Add the provided content as a new entry in the object store
   await store.add({ content });
-  await tx.complete;
+  await tx.complete; // Complete the transaction
+  
   console.log('Content added to the database:', content);
 };
 
+// Function to retrieve data from the database
 export const getDb = async () => {
-  const db = await initDB();
-  const tx = db.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
+  const db = await initDB(); // Initialize the database
+  const tx = db.transaction('jate', 'readonly'); // Start a transaction in 'readonly' mode
+  const store = tx.objectStore('jate'); // Get the object store named 'jate'
+  
+  // Retrieve all the content stored in the object store
   const content = await store.getAll();
-  await tx.complete;
+  await tx.complete; // Complete the transaction
+  
   console.log('Retrieved content from the database:', content);
-  return content;
+  return content; // Return the retrieved content
 };
 
-initDB();
+initDB(); // Initialize the database when the file is executed
